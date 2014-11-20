a74558e7ac6350bcb639249453bc37c124fa5dbe
tlallart <thomas.lallart@gmail.com>
2014-11-20 18:39:09 +0100

    Update README and extends NodeJS server to all http requests

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ angular-first-steps
 ===================
 
 First steps with AngularJS and NodeJS
+
+
+Using
+=====
+
+1) Clone or download zip of this repository
+
+2) Install Node.js and SQLite3 module :
+sudo apt-get install nodejs npm
+sudo npm -g install sqlite3
+
+OR
+
+Download Node.js binaries or sources
+Add sqlite3 module : npm -g install sqlite3
+
+3) Start Node.js server
+From angular-first-step folder previously downloaded, run 
+node logs_server.js
+
+4) From your browser go to http://localhost:3000/index.html
--- logs_server.js
+++ logs_server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var path = require("path");
 var sqlite3 = require('sqlite3').verbose();
 
 var db = new sqlite3.Database('logs.db');
@@ -34,6 +35,28 @@ var app = http.createServer(function (req, res) {
 		stmt.run(timestamp, queryData.username, queryData.msg);
 		stmt.finalize();
 		res.end(JSON.stringify({ time: timestamp}));
+	} else {
+		var my_path = url.parse(req.url).pathname;
+		var full_path = path.join(process.cwd(), my_path);
+		fs.exists(full_path, function (exists) {
+			if (!exists) {
+				res.writeHeader(404, {"Content-Type": "text/plain"});  
+				res.write("404 Not Found\n");  
+				res.end();
+			} else {
+				fs.readFile(full_path, "binary", function(err, file) {  
+					if (err) {  
+						res.writeHeader(500, {"Content-Type": "text/plain"});  
+						res.write(err + "\n");  
+						res.end();  
+			     		} else {
+						res.writeHeader(200);  
+					        res.write(file, "binary");  
+					        res.end();
+					}
+				});
+			}
+		});
 	};
 });
 
